120c26b72215b3a556a4ca08258d69b2b0e491bd
rhonnyjaimes <rhonnyjaimes2002@gmail.com>
2024-08-25 00:37:13 +0200

    actualizacion de la base de datos

--- gestion_estudiantes.sql
+++ gestion_estudiantes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-08-2024 a las 16:54:48
+-- Tiempo de generación: 24-08-2024 a las 23:44:16
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -43,7 +43,7 @@ CREATE TABLE `estudiantes` (
 --
 
 INSERT INTO `estudiantes` (`id`, `nombre`, `cedula`, `edad`, `carrera`, `correo`, `inscrito`, `deuda`) VALUES
-(1, 'Rhonny Jaimes Gonzalez', 'V-29.718.095', 22, 'Ingeniería en Computación', 'rhonny@uvm.com', 0, 100.00),
+(1, 'Rhonny Jaimes Gonzalez Jaimez', 'V-29.718.095', 22, 'Ingeniería en Computación', 'rhonny@uvm.com', 1, 100.00),
 (2, 'Ana Perez', 'V-26.718.095', 24, 'Ingeniería en Computación', 'ana.perez@uvm.com', 1, 150.00),
 (3, 'Carlos Hernandez', 'V-28.718.095', 21, 'Derecho', 'carlos.hernandez@uvm.com', 1, 0.00),
 (4, 'Luis Alberto Fernández', 'V-27.318.091', 23, 'Ingeniería en Computación', 'luis.fernandez@uvm.com', 1, 50.00),
@@ -51,9 +51,20 @@ INSERT INTO `estudiantes` (`id`, `nombre`, `cedula`, `edad`, `carrera`, `correo`
 (6, 'José Manuel Rojas', 'V-30.718.123', 20, 'Administración', 'jose.rojas@uvm.com', 1, 200.00),
 (7, 'Patricia Gómez', 'V-29.118.119', 21, 'Medicina', 'patricia.gomez@uvm.com', 0, 0.00),
 (8, 'Raúl González', 'V-28.518.120', 24, 'Ingeniería en Computación', 'raul.gonzalez@uvm.com', 1, 100.00),
-(9, 'Elena García', 'V-26.918.121', 23, 'Derecho', 'elena.garcia@uvm.com', 0, 0.00),
-(11, 'Norlys', '4932412', 30, 'Contaduria', 'norlys@uvm.com', 1, 1000.00),
-(12, 'Antonio Calderon', 'V-30.123.452', 36, 'Ingeniería en Computación', 'antonio@uvm.com', 0, 100.00);
+(9, 'Elena García', 'V-26.918.121', 23, 'Derecho', 'elena.garcia@uvm.com', 0, 0.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` enum('admin','editor') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
@@ -66,6 +77,13 @@ ALTER TABLE `estudiantes`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -73,7 +91,13 @@ ALTER TABLE `estudiantes`
 -- AUTO_INCREMENT de la tabla `estudiantes`
 --
 ALTER TABLE `estudiantes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
